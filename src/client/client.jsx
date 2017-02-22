75126e4dae2c7db9edad9abe3dd31aa5f759d2dc
Sean Mailander <me@seanmailander.com>
2017-02-21 18:37:24 -0800

    Clean up docs, add relative paths, remove router

--- src/client/client.jsx
+++ src/client/client.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
 
 import App from './components/app';
 
 
 ReactDOM.render(
-  <Router history={ browserHistory }>
-    <Route path='/' component={ App } />
-  </Router>,
+  <App />,
   document.getElementById('app'),
 );

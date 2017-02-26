9ba525d82489609294ae927fb318560d4f38606a
Sean Mailander <me@seanmailander.com>
2017-02-25 18:25:23 -0800

    Add initialization to build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 curl https://www.wordgamedictionary.com/sowpods/download/sowpods.txt -o data/sowpods.txt
+
+./node_modules/.bin/babel-node -e 'require("./src/server/lib/initializeData.js").getFlatWordList();';

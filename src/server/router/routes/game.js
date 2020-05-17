345a0c37955418187920f219eb26167f49da55a3
Sean Mailander <seanmailander@users.noreply.github.com>
2020-05-16 23:23:17 -0700

    Simplify request param handling

--- src/server/router/routes/game.js
+++ src/server/router/routes/game.js
@@ -10,23 +10,8 @@ router.get('/randomGame', (req, res, next) => {
 });
 
 router.get('/wordsForGame', (req, res, next) => {
-  req.check('board', 'board must exist').notEmpty();
-  if (req.params.board) {
-    req.sanitize('board').alphaOnly();
-    req.sanitize('board').toLowerCase();
-    req.assert('board', 'board must have 25 letters').len(25, 25);
-  }
-
-  const errors = req.validationErrors() || [];
-  if (errors.length) {
-    next(util.inspect(errors));
-  } else {
-        // all validation passed
-    const gameBoard = req.params.board;
-
-
-    res.json(getWordsForGame(gameBoard));
-  }
+  const gameBoard = req.query.board;
+  res.json(getWordsForGame(gameBoard));
 });
 
 export default router;

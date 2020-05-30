8876a3b3d0632ad52e8b7b377dac5415673b9abc
Sean Mailander <seanmailander@users.noreply.github.com>
2020-05-30 12:53:09 -0700

    Clean up stale dependencies (#5)
    * Bump react
     - remove bluebird
     - add octocat
    
    * Replace superagent with fetch
    
    * Fix proptypes

--- src/client/services/letterpresser.js
+++ src/client/services/letterpresser.js
@@ -1,18 +1,8 @@
-import * as BaseAPI from './baseApi';
-
-export const getWordsForGameRequest = board =>
-    BaseAPI.generateApiGet(`/api/wordsForGame?board=${board}`)
-        .then(response => response.map(word => word.toUpperCase()));
-
-export const getRandomGameRequest = () =>
-    BaseAPI.generateApiGet('/api/randomGame')
-        .then(response => response.toUpperCase());
-
-
 export const getRandomGame = () =>
-    BaseAPI.call(getRandomGameRequest.bind(this));
+    fetch('/api/randomGame')
+    .then(response => response.json())
 
 export const getWordsForGame = board =>
-    BaseAPI.call(getWordsForGameRequest.bind(this, board));
-
-
+    fetch(`/api/wordsForGame?board=${board}`)
+      .then(response => response.json())
+      .then(response => response.map(word => word.toUpperCase()));

7321a6ebb20814b4364419b0e37e089d958debb9
Sean Mailander <seanmailander@users.noreply.github.com>
2020-05-30 12:48:33 -0700

    Fix proptypes

--- src/client/components/board.jsx
+++ src/client/components/board.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import { range } from '../../common/util';
 import { StartingBoardState, LetterState, applyLockingToBoard, applyMovesToBoard } from '../../common/boardOperations';

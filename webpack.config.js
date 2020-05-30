1c624e7d1157e6f4f17184a7b0cd0b49ab51c8e5
Sean Mailander <seanmailander@users.noreply.github.com>
2020-05-30 14:52:37 -0700

    Bump dev packages

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,38 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    cache: true,
-    entry: {
-      client: './src/client/client.jsx',
-    },
-    output: {
-      filename: '[name].js',
-      chunkFilename: '[name].js',
-      path: path.resolve(__dirname, 'public'),
-    },
-    devtool: 'eval',
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env']
-            }
-          }
-        }
-      ],
-    },
-    resolve: {
-      extensions: ['.js', '.jsx'],
-    },
-    plugins: [
-      new CopyWebpackPlugin([
-        // {output}/file.txt
-        { from: './src/client/assets', to: './' },
-      ]),
+  cache: true,
+  entry: {
+    client: './src/client/client.jsx',
+  },
+  output: {
+    filename: '[name].js',
+    chunkFilename: '[name].js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devtool: 'eval',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './src/client/assets', to: './' },
+      ],
+    }),
+  ],
 };
